686a794cf2b3cf3f19baa67f4ca78e385922e5d4
herviansyahreza <82496908+herviansyahreza@users.noreply.github.com>
2022-11-07 17:33:26 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import{BrowserRouter,Routes, Route} from 'react-router-dom'
+import {Main} from './components/Main'
+import { Details } from './components/Details';
+import {Search} from './components/Search';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main/>}></Route>
+        <Route path='/book/:isbn' element={<Details/>}></Route>
+        <Route path='/:query' element={<Search/>}></Route>
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App;
